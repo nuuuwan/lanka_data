f9025a1ea0ae57965ebfb30054ad440acc9bbd4f
nuuuwan <nuuuwan@gmail.com>
2026-05-28 18:07:00 +0530

    feat: add BasicWhat import to enhance command handling in what module

--- src/lanka_data/what/__init__.py
+++ src/lanka_data/what/__init__.py
@@ -1,6 +1,7 @@
 # lanka_data.what (auto generate by build_inits.py)
 # flake8: noqa: F408
 
+from lanka_data.what.BasicWhat import BasicWhat
 from lanka_data.what.census2024 import Census2024
 from lanka_data.what.gig2 import GIG2, Census2012, Elections
 from lanka_data.what.What import What
