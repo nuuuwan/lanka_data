2da7f3f0669a0148d476929fcc03e01ef617ae87
nuuuwan <nuuuwan@gmail.com>
2026-06-06 14:19:00 +0530

    Refactor test script for improved readability and maintainability

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,4 @@
-python3 workflows/examples_build.py && python3 -m pytest -v -p no:warnings "$*" && python3 workflows/readme_build.py
+rm -rf /tmp/lanka_data \
+    && python3 workflows/examples_build.py \
+    && python3 -m pytest -v -p no:warnings "$@" \
+    && python3 workflows/readme_build.py
