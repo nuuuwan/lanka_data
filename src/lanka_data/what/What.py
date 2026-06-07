85e9ff75fa23195425cb033b786cbf09e780d365
nuuuwan <nuuuwan@gmail.com>
2026-06-07 10:18:50 +0530

    Implement data mapping and aggregation methods in What class for improved data handling

--- src/lanka_data/what/What.py
+++ src/lanka_data/what/What.py
@@ -29,3 +29,70 @@ class What:
     @classmethod
     def get_values(cls, data):
         return data.get("values")
+
+    @classmethod
+    def get_where_to_what_id_map(cls, regions) -> dict:
+        idx = {}
+        for region in regions.raw_region_data_list:
+            region_id = region["id"]
+            current_ids = region.get("current_ids", [region_id])
+            idx[region_id] = current_ids
+        return idx
+
+    @classmethod
+    def _get_mapped_data_for_region(
+        cls, region_id, current_ids, raw_data_idx, raw_region_data_idx
+    ):
+        raw_data_list_for_region = []
+        for current_id in current_ids:
+            data_for_current = raw_data_idx.get(current_id)
+            if not data_for_current:
+                raise ValueError(
+                    f"No data found for region_id={current_id}"
+                    + f" (mapped from {region_id})."
+                )
+            raw_data_list_for_region.append(data_for_current)
+
+        if not raw_data_list_for_region:
+            raise ValueError(
+                f"No data found for region_id={region_id}"
+                + f" with current_ids={current_ids}."
+            )
+
+        cleaned_data_list_for_region = [
+            cls.clean(d) for d in raw_data_list_for_region
+        ]
+        aggr_data = cls.get_aggregated_value_data(cleaned_data_list_for_region)
+        raw_data_for_region = raw_region_data_idx[region_id]
+        mapped_data = (
+            dict(
+                region_id=region_id,
+                region_name=raw_data_for_region["name"],
+            )
+            | raw_region_data_idx[region_id]
+            | aggr_data
+        )
+        del mapped_data["id"]
+        del mapped_data["name"]
+        return mapped_data
+
+    def get_data_list(self, regions) -> list[dict]:
+        raw_data_list = self.get_source_data_list()
+        raw_data_idx = {
+            d["region_id"]: d for d in raw_data_list if "region_id" in d
+        }
+
+        raw_region_data_idx = {
+            r["id"]: r for r in regions.raw_region_data_list
+        }
+
+        mapped_data_list = []
+        for region_id, current_ids in self.get_where_to_what_id_map(
+            regions
+        ).items():
+            mapped_data = self._get_mapped_data_for_region(
+                region_id, current_ids, raw_data_idx, raw_region_data_idx
+            )
+            mapped_data_list.append(mapped_data)
+
+        return mapped_data_list
--- src/lanka_data/what/gig2/GIG2.py
+++ src/lanka_data/what/gig2/GIG2.py
@@ -27,34 +27,3 @@ class GIG2(What):
         )
         data_list = WWW(url).read_tsv()
         return data_list
-
-    def get_data_list(self, regions) -> list[dict]:
-        base_data_list = self.get_source_data_list()
-        base_data_idx = {d["entity_id"]: d for d in base_data_list}
-
-        region_idx = {r["id"]: r for r in regions.raw_region_data_list}
-        region_to_current_ids = regions.region_to_current_ids
-        merged_data_list = []
-        for region_id, current_ids in region_to_current_ids.items():
-            log.debug(f"{region_id} -> {current_ids}")
-            data_list = []
-            for current_id in current_ids:
-                d = base_data_idx.get(current_id)
-                if d is not None:
-                    data_list.append(d)
-
-            if not data_list:
-                continue
-            cleaned_data_list = [self.get_custom_data(d) for d in data_list]
-            aggr_data = self.get_aggregated_value_data(cleaned_data_list)
-            region_data = (
-                dict(
-                    region_id=region_id,
-                    region_name=region_idx[region_id]["name"],
-                    current_ids=current_ids,
-                )
-                | aggr_data
-            )
-            merged_data_list.append(region_data)
-
-        return merged_data_list
