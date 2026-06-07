4562676c252879bd1fb83704bd9b5c595c00952a
nuuuwan <nuuuwan@gmail.com>
2026-06-07 10:53:03 +0530

    Rename 'year' to 'historical_year' for clarity in Where class

--- src/lanka_data/where/Where.py
+++ src/lanka_data/where/Where.py
@@ -1,10 +1,13 @@
 class Where:
-    def __init__(self, title: str, year: str):
+    def __init__(self, title: str, historical_year: str):
         self.title = title
-        self.year = year
+        self.historical_year = historical_year
 
     def get_title(self):
         return self.title
 
     def __str__(self):
-        return f"{self.__class__.__name__}({self.get_title()}/{self.year})"
+        return (
+            f"{self.__class__.__name__}"
+            + f"({self.get_title()}/{self.historical_year})"
+        )
