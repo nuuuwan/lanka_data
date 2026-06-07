8cf28c57bb1e4edaf67bb295025cba73e26a18da
nuuuwan <nuuuwan@gmail.com>
2026-06-07 10:45:57 +0530

    Update output JSON files to reflect error messages and add region metadata
    - Changed output JSON files to return error messages for missing data instead of result objects.
    - Added region type and history year fields to various region entries in the JSON files.
    - Updated test script to include new workflow calls for specific region queries.

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,7 @@
 git add src
-python3 workflows/examples_build.py \
+python3 workflows/single.py "LK" \
+&& python3 workflows/single.py "LK-71:district/Map" \
+&& python3 workflows/single.py "LK-pre1959:district/Religion/2012/Map" \
+&& python3 workflows/examples_build.py \
     && python3 -m pytest -v -p no:warnings "$@" \
     && python3 workflows/readme_build.py
