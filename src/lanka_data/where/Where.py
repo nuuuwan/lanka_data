1a263a4c82075bda88f3a63b202a694df56279a4
nuuuwan <nuuuwan@gmail.com>
2026-06-07 08:31:55 +0530

    Update Where class constructor to include year and modify string representation

--- src/lanka_data/where/Where.py
+++ src/lanka_data/where/Where.py
@@ -1,9 +1,10 @@
 class Where:
-    def __init__(self, title):
+    def __init__(self, title: str, year: str):
         self.title = title
+        self.year = year
 
     def get_title(self):
         return self.title
 
     def __str__(self):
-        return f"{self.__class__.__name__}({self.get_title()})"
+        return f"{self.__class__.__name__}({self.get_title()}/{self.year})"
