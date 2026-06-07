222b870dc03aaf477c1d550c0a1b4af0f9edad75
nuuuwan <nuuuwan@gmail.com>
2026-06-07 18:53:30 +0530

    Refactor test.sh to streamline git commands and ensure proper execution flow

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,13 @@
 git add src
+git commit -m "Various src changes"
+
 rm -rf /tmp/lanka_data/cache
 python3 workflows/examples_build.py \
     && python3 workflows/readme_build.py \
     && python3 -m pytest -x -v -p no:warnings "$@" 
+
+
+git add .
+git commit -m "Updated examples and readme"
+
+git push origin main
