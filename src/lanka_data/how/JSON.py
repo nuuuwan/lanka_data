352a9d4195781fc7ec4027f131b763fce7be35f9
nuuuwan <nuuuwan@gmail.com>
2026-06-07 16:04:23 +0530

    Refactor descriptions in JSON and Map classes for consistency and clarity

--- src/lanka_data/how/JSON.py
+++ src/lanka_data/how/JSON.py
@@ -3,7 +3,7 @@ from lanka_data.how.How import How
 
 class JSON(How):
     def get_description(self):
-        return "Raw data in JSON output."
+        return "JSON data"
 
     def get_inner(self, where, what, when):
         return self.get_data(where, what, when)
--- src/lanka_data/how/Map.py
+++ src/lanka_data/how/Map.py
@@ -4,7 +4,7 @@ from lanka_data.how.MapUtils import MapUtils
 
 class Map(How):
     def get_description(self):
-        return "Geographical map visualization of the data."
+        return "Geographical map visualization"
 
     def get_inner(self, where, what, when):
         return MapUtils.draw_map(where, what, when, self)
