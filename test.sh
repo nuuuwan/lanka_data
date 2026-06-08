25538ec2b17ea736379bac60f318442de525b39a
nuuuwan <nuuuwan@gmail.com>
2026-06-08 12:16:13 +0530

    Update test.sh to include git add and commit for examples and README.md

--- test.sh
+++ test.sh
@@ -5,3 +5,6 @@ python3 workflows/examples_build.py \
     && python3 -m pytest -x -v -p no:warnings "$@" 
 
 
+git add examples
+git add README.md
+git commit -m "Ran test.sh to update examples and README.md"
