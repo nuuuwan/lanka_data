1b0be921e03250b1393b8c24cc249a0599462768
nuuuwan <nuuuwan@gmail.com>
2026-06-09 20:22:27 +0530

    Add cartogram output for Ethnicity LK-53 and update geometry processing in DCNUtils

--- src/utils_future/DCNUtils.py
+++ src/utils_future/DCNUtils.py
@@ -210,5 +210,5 @@ class DCNUtils:
         features = geojson["features"]
         DCNUtils._run_features(features, region_id_to_weight, n_iterations)
         result = gdf.copy()
-        result["geometry"] = [shape(f["geometry"]) for f in features]
+        result["geometry"] = [shape(f["geometry"]).buffer(0) for f in features]
         return result
