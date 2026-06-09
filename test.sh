9246cf3e6405c6f8536bc889b9a45e8442fdc824
nuuuwan <nuuuwan@gmail.com>
2026-06-09 12:37:52 +0530

    Remove git add and commit commands from test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,3 @@ python3 workflows/examples_build.py \
     && python3 -m pytest -x -v -p no:warnings "$@" 
 
 
-git add examples
-git add images
-git add README.md
-git commit -m "Ran test.sh to update examples and README.md"
