1d4927984721186222b85962e6c169df002fc69e
nuuuwan <nuuuwan@gmail.com>
2026-06-09 08:34:07 +0530

    Refactor get_lines_for_usage method for improved readability and structure

--- src/lanka_data/readme/ReadMe.py
+++ src/lanka_data/readme/ReadMe.py
@@ -34,42 +34,47 @@ class ReadMe:
         return lines
 
     def get_lines_for_usage(self):
-        return [
-            "## Usage",
-            "",
-            "### Run Code",
-            "",
-            "```python",
-            "from lanka_data import Db",
-            "",
-            "",
-            'db = Db("<cmd>")',
-            "output = db.run()",
-            "print(output)",
-            "",
-            "```",
-            "",
-            "### workflows/single.py",
-            "",
-            "Runs single command.",
-            "",
-            "```bash",
-            "python workflows/single.py <cmd>",
-            "```",
-            "",
-            "### workflows/console.py",
-            "",
-            "Console tool for running commands.",
-            "",
-            "```bash",
-            "python workflows/console.py <cmd>",
-            "",
-            "/Where/What/When/How",
-            "",
-            "> /<cmd>",
-            "```",
-            "",
-        ]
+        return (
+            ["## Usage", ""]
+            + [
+                "### Run Code",
+                "",
+                "```python",
+                "from lanka_data import Db",
+                "",
+                "",
+                'db = Db("<cmd>")',
+                "output = db.run()",
+                "print(output)",
+                "",
+                "```",
+                "",
+            ]
+            + [
+                "### workflows/single.py",
+                "",
+                "Runs single command.",
+                "",
+                "```bash",
+                "python workflows/single.py <cmd>",
+                "```",
+                "",
+            ]
+            + [
+                "### workflows/console.py",
+                "",
+                "Console tool for running commands.",
+                "",
+                "```bash",
+                "python workflows/console.py <cmd>",
+                "",
+                "/Where/What/When/How",
+                "",
+                "> /<cmd>",
+                "```",
+                "",
+            ]
+        )
 
     @staticmethod
     def get_lines_for_example_title(i_group_name, i_cmd, cmd, result):
