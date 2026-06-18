9ae49e331fb5d7c1ba788dfa7fbdbb4cce40816d
nuuuwan <nuuuwan@gmail.com>
2026-06-18 08:14:30 +0530

    Refactor FontUtils install_font method for improved string formatting and readability.

--- src/lanka_data/api/how/map/FontUtils.py
+++ src/lanka_data/api/how/map/FontUtils.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 class FontUtils:
     @staticmethod
     def install_font(font_family: str):
-        font_file_path = os.path.join('fonts', font_family + '.ttf')
+        font_file_path = os.path.join("fonts", font_family + ".ttf")
         if not os.path.exists(font_file_path):
             raise FileNotFoundError(
-                f"Font file '{font_file_path}' not found for font family '{font_family}'."
+                f"'{font_file_path}' not found for '{font_family}'."
             )
         fm.fontManager.addfont(font_file_path)
         plt.rcParams["font.family"] = font_family
