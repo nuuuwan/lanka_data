21ec655415887583b83e5ba59d412d719f8e0f1a
nuuuwan <nuuuwan@gmail.com>
2026-06-18 08:48:59 +0530

    Remove unnecessary conditional logic for handling 'when_cmd' in CommandLoaderMixin

--- src/lanka_data/command/CommandLoaderMixin.py
+++ src/lanka_data/command/CommandLoaderMixin.py
@@ -17,12 +17,5 @@ class CommandLoaderMixin:
             )
 
         what_cmd, when_cmd, where_cmd, how_cmd = tokens
-
-        if when_cmd == "2012":
-            if "-pre" not in where_cmd:
-                tokens = where_cmd.split(":")
-                if len(tokens) == 2 and tokens[1] == "dsd":
-                    where_cmd = tokens[0] + "-pre2019:" + ":".join(tokens[1:])
-
         log.debug(f"{what_cmd=}, {when_cmd=}, {where_cmd=}, {how_cmd=}")
         return cls(where_cmd, what_cmd, when_cmd, how_cmd)
