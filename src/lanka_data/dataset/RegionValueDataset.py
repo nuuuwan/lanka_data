ef14dc592fbaf39b5f7c723d4097d8fa53ddbb9e
nuuuwan <nuuuwan@gmail.com>
2026-06-19 18:47:50 +0530

    Implement expand method in RegionValueDataset to calculate percentage values and update get_data_table to include expanded data.

--- src/lanka_data/dataset/RegionValueDataset.py
+++ src/lanka_data/dataset/RegionValueDataset.py
@@ -2,9 +2,20 @@ from lanka_data.dataset.Dataset import Dataset
 
 
 class RegionValueDataset(Dataset):
+    PCT_VALUE_PRECISION = 4
+
     def __init__(self, region_ids):
         Dataset.__init__(self)
         self.region_ids = region_ids
+
+    def expand(self, data):
+        total_value = data["total_value"]
+        pct_values = {
+            k: round(v / total_value, self.PCT_VALUE_PRECISION)
+            for k, v in data["values"].items()
+        }
+        data["pct_values"] = pct_values
+        return data
 
     def get_data_table(self):
         complete_data_table = self.get_complete_data_table()
@@ -17,4 +28,7 @@ class RegionValueDataset(Dataset):
             filtered_data_table,
             key=lambda row: self.region_ids.index(row["region_id"]),
         )
-        return sorted_data_table
+        expanded_data_table = [
+            self.expand(data) for data in sorted_data_table
+        ]
+        return expanded_data_table
