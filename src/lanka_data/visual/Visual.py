02b89210e7b737a41c98b8b65224b5a06defbf30
nuuuwan <nuuuwan@gmail.com>
2026-06-19 09:40:06 +0530

    Enhance Visual class: improve string representation and add logging in from_commmand_and_datasets method

--- src/lanka_data/visual/Visual.py
+++ src/lanka_data/visual/Visual.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 
 from lanka_data.dataset.Dataset import Dataset
+from utils_future import Log
+
+log = Log("Visual")
 
 
 @dataclass
@@ -9,8 +12,16 @@ class Visual:
     how_cmd: str
 
     def __str__(self):
-        return f"Visual({self.datasets}/{self.how_cmd})"
+        return f"Visual({self.datasets} -> {self.how_cmd})"
+
+    def __repr__(self):
+        return self.__str__()
 
     @classmethod
     def from_commmand_and_datasets(cls, command, datasets):
-        return cls(datasets=datasets, how_cmd=command.how_cmd)
+        visual = cls(datasets=datasets, how_cmd=command.how_cmd)
+        log.debug(f"Built {visual}")
+        return visual
+
+    def build(self):
+        pass
