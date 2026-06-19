e4f159a08205d3c29d54de912e0cec6fb4670060
nuuuwan <nuuuwan@gmail.com>
2026-06-19 13:30:16 +0530

    Implement __str__ method in Dataset class to return class name

--- src/lanka_data/dataset/Dataset.py
+++ src/lanka_data/dataset/Dataset.py
@@ -6,7 +6,7 @@ log = Log("Dataset")
 class Dataset:
 
     def __str__(self):
-        raise NotImplementedError("Subclasses must implement __str__()")
+        return self.__class__.__name__
 
     def __repr__(self):
         return self.__str__()
