e352eee10b37b941255fe5308af9aecefe29adb5
nuuuwan <nuuuwan@gmail.com>
2026-06-19 12:33:14 +0530

    Refactor imports in single.py: update Dataset and Visual references to use DatasetFactory and VisualFactory

--- workflows/single.py
+++ workflows/single.py
@@ -1,12 +1,12 @@
 import sys
 
-from lanka_data import Command, Dataset, Visual
+from lanka_data import Command, DatasetFactory, VisualFactory
 
 
 def main(command_str):
     command = Command.from_str(command_str)
-    datasets = Dataset.list_from_command(command)
-    visual = Visual.from_commmand_and_datasets(command, datasets)
+    datasets = DatasetFactory.list_from_command(command)
+    visual = VisualFactory.from_commmand_and_datasets(command, datasets)
     visual.build()
 
 
