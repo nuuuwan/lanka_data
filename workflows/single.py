271593227a87ef79d524cd2f241effa51befcc64
nuuuwan <nuuuwan@gmail.com>
2026-06-20 10:40:39 +0530

    Fix output formatting in main function: print visual build result as JSON

--- workflows/single.py
+++ workflows/single.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from lanka_data import Command, DatasetFactory, VisualFactory
@@ -5,9 +6,10 @@ from lanka_data import Command, DatasetFactory, VisualFactory
 
 def main(command_str):
     command = Command.from_str(command_str)
+
     datasets = DatasetFactory.list_from_command(command)
     visual = VisualFactory.from_commmand_and_datasets(command, datasets)
-    visual.build()
+    print(json.dumps(visual.build(), indent=2))
 
 
 if __name__ == "__main__":
