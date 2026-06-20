55516c4b97d9d66d6dc8ad419d2484d8de4e3d01
nuuuwan <nuuuwan@gmail.com>
2026-06-20 19:50:20 +0530

    refactor: Simplify main function by replacing command handling with CommandRunner execution

--- workflows/single.py
+++ workflows/single.py
@@ -1,15 +1,12 @@
 import json
 import sys
 
-from lanka_data import Command, DatasetFactory, VisualFactory
+from lanka_data.command.CommandRunner import CommandRunner
 
 
 def main(command_str):
-    command = Command.from_str(command_str)
 
-    datasets = DatasetFactory.list_from_command(command)
-    visual = VisualFactory.from_commmand_and_datasets(command, datasets)
-    print(json.dumps(visual.build(), indent=2))
+    print(json.dumps(CommandRunner.run(command_str), indent=2))
 
 
 if __name__ == "__main__":
