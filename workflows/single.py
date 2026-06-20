83d326ef3a2c5e2611fff2a553402ea7744b8583
nuuuwan <nuuuwan@gmail.com>
2026-06-20 21:10:08 +0530

    feat: Enhance main function to open image path from command output

--- workflows/single.py
+++ workflows/single.py
@@ -1,12 +1,16 @@
 import json
+import os
 import sys
 
 from lanka_data.command.CommandRunner import CommandRunner
 
 
 def main(command_str):
-
-    print(json.dumps(CommandRunner.run(command_str), indent=2))
+    output = CommandRunner.run(command_str)
+    image_path = output.get("result", {}).get("image_path")
+    if image_path:
+        os.system(f"open {image_path}")
+    print(json.dumps(output, indent=2))
 
 
 if __name__ == "__main__":
