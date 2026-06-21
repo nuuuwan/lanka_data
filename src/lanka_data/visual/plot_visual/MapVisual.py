bf481ff5e6cd2f94410b8f4b825cb3d4a97322df
nuuuwan <nuuuwan@gmail.com>
2026-06-21 08:34:57 +0530

    refactor: Simplify _get_gdf_region method signature and improve region labeling logic in draw method

--- src/lanka_data/visual/plot_visual/MapVisual.py
+++ src/lanka_data/visual/plot_visual/MapVisual.py
@@ -12,7 +12,7 @@ class MapVisual(PlotVisual):
     DEFAULT_EDGE_WIDTH = 0.2
     MAX_REGIONS_TO_LABEL = 30
 
-    def _get_gdf_region(self, dataset, region_color_map, value_to_color):
+    def _get_gdf_region(self, dataset, region_color_map):
         data_list = dataset.get_data_table()
         gdf_region = GeoData.get_geopandas_dataframe(
             data_list, "Cartogram" in self.how_cmd
@@ -27,7 +27,8 @@ class MapVisual(PlotVisual):
         ).unpack()
 
         gdf_region = self._get_gdf_region(
-            dataset, region_color_map, value_to_color
+            dataset,
+            region_color_map,
         )
 
         gs = fig.add_gridspec(
@@ -46,7 +47,8 @@ class MapVisual(PlotVisual):
             edgecolor=self.DEFAULT_EDGE_COLOR,
             linewidth=self.DEFAULT_EDGE_WIDTH,
         )
-        Label.draw(gdf_region, ax)
+        if len(gdf_region) <= self.MAX_REGIONS_TO_LABEL:
+            Label.draw(gdf_region, ax)
         Legend.draw(value_to_color, legend_ax)
         ax.set_axis_off()
         Text.plot(
