82c9f8feb135e99fe0b7426d81022471b11ed579
nuuuwan <nuuuwan@gmail.com>
2026-06-21 08:07:44 +0530

    refactor: Extract region data retrieval into separate method and apply timer decorator to draw method

--- src/lanka_data/visual/plot_visual/MapVisual.py
+++ src/lanka_data/visual/plot_visual/MapVisual.py
@@ -4,6 +4,7 @@ from lanka_data.visual.plot.Legend import Legend
 from lanka_data.visual.plot.map.GeoData import GeoData
 from lanka_data.visual.plot.Text import Text
 from lanka_data.visual.plot_visual.PlotVisual import PlotVisual
+from utils_future import timer
 
 
 class MapVisual(PlotVisual):
@@ -11,16 +12,23 @@ class MapVisual(PlotVisual):
     DEFAULT_EDGE_WIDTH = 0.2
     MAX_REGIONS_TO_LABEL = 30
 
-    def draw(self, dataset, fig):
+    def _get_gdf_region(self, dataset, region_color_map, value_to_color):
         data_list = dataset.get_data_table()
-        len(data_list)
         gdf_region = GeoData.get_geopandas_dataframe(
             data_list, "Cartogram" in self.how_cmd
         ).copy()
+        gdf_region["color"] = gdf_region["region_id"].map(region_color_map)
+        return gdf_region
+
+    @timer
+    def draw(self, dataset, fig):
         region_color_map, value_to_color = ColorSpecFactory.get_color_spec(
             dataset, self.how_cmd
         ).unpack()
-        gdf_region["color"] = gdf_region["region_id"].map(region_color_map)
+
+        gdf_region = self._get_gdf_region(
+            dataset, region_color_map, value_to_color
+        )
 
         gs = fig.add_gridspec(
             1,
@@ -40,7 +48,6 @@ class MapVisual(PlotVisual):
         )
         Label.draw(gdf_region, ax)
         Legend.draw(value_to_color, legend_ax)
-
         ax.set_axis_off()
         Text.plot(
             fig,
