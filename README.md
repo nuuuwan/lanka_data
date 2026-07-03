b038eafb663b6e3b087dc40ff00574cb065c54da
nuuuwan <nuuuwan@gmail.com>
2026-07-03 17:33:09 +0530

    feat: Enhance README content for clarity and detail

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Lanka Data
 
-This repo implements a simple interface to query data about Sri Lanka.
+This repo implements "one API to rule them all": a single interface that can express *any* query to access public data about Sri Lanka 🇱🇰.
+
+## Design Philosophy
+
+See [README.philosophy.md](README.philosophy.md).
 
 ## Data Sources
 
--- src/lanka_data/readme/ReadMe.py
+++ src/lanka_data/readme/ReadMe.py
@@ -21,8 +21,13 @@ class ReadMe(
             [
                 "# Lanka Data",
                 "",
-                "This repo implements a simple interface"
-                + " to query data about Sri Lanka.",
+                'This repo implements "one API to rule them all":'
+                + " a single interface that can express"
+                + " *any* query to access public data about Sri Lanka 🇱🇰.",
+                "",
+                "## Design Philosophy",
+                "",
+                "See [README.philosophy.md](README.philosophy.md).",
                 "",
             ]
             + self.get_lines_for_sources(output_idx)
