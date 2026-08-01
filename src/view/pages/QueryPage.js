642965ca0bf0741a99a38e3802468651f6f80f45
nuuuwan <nuuuwan@gmail.com>
2026-08-01 07:56:06 +0530

    Refactor QueryPage component to include data fetching and loading state

--- src/view/pages/QueryPage.js
+++ src/view/pages/QueryPage.js
@@ -1,11 +1,31 @@
 import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
-
+import { Typography, Box, CircularProgress } from "@mui/material";
+import { useState, useEffect } from "react";
+import Census2024 from "../../nonview/core/Census2024.js";
 export default function QueryPage() {
   const { "*": queryStr } = useParams();
+
+  const [lankaData, setLankaData] = useState(null);
+  useEffect(() => {
+    async function fetch() {
+      const lankaData = await Census2024.getLankaDataForQuery(queryStr);
+      setLankaData(lankaData);
+    }
+    fetch();
+  }, [queryStr]);
+
   return (
-    <Typography variant="body1" sx={{ mt: 2 }}>
-      {queryStr}
-    </Typography>
+    <Box>
+      <Typography variant="h4" sx={{ mt: 2 }}>
+        {queryStr}
+      </Typography>
+      {lankaData === null ? (
+        <CircularProgress sx={{ mt: 2 }} />
+      ) : (
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          {JSON.stringify(lankaData, null, 2)}
+        </Typography>
+      )}
+    </Box>
   );
 }
