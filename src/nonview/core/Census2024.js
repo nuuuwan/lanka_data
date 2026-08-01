a3ec696cb43fb6810719066a44c517ead5c72e4a
nuuuwan <nuuuwan@gmail.com>
2026-08-01 07:56:02 +0530

    Add Census2024 class for fetching census metadata and data queries

---
+++ src/nonview/core/Census2024.js
@@ -0,0 +1,32 @@
+import WWW from "../../nonview/base/WWW.js";
+
+export default class Census2024 {
+  static URL_REPO = "https://raw.githubusercontent.com/nuuuwan/lk_census_2024";
+  static URL_BASE = Census2024.URL_REPO + "/refs/heads/main";
+
+  static URL_METADATA =
+    Census2024.URL_BASE + "/metadata/lanka_data.metadata.json";
+
+  static async getMetadata() {
+    return await WWW.json(Census2024.URL_METADATA);
+  }
+
+  static async getMetadataForQuery(queryStr) {
+    const metadata = await Census2024.getMetadata();
+    return metadata[queryStr] || [];
+  }
+
+  static async getLankaDataForPartialPath(partialPath) {
+    const url = Census2024.URL_BASE + "/" + partialPath;
+    return await WWW.json(url);
+  }
+
+  static async getLankaDataForQuery(queryStr) {
+    const metadataForQuery = await Census2024.getMetadataForQuery(queryStr);
+    return await Promise.all(
+      metadataForQuery.map(async (partialPath) => {
+        return await Census2024.getLankaDataForPartialPath(partialPath);
+      }),
+    );
+  }
+}
