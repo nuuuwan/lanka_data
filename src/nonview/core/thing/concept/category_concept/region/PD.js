52b44505f836ac8f2987799877afe027dd90bc82
nuuuwan <nuuuwan@gmail.com>
2026-08-02 14:41:52 +0530

    feat: simplify GND and PD classes by removing unnecessary static properties

--- src/nonview/core/thing/concept/category_concept/region/PD.js
+++ src/nonview/core/thing/concept/category_concept/region/PD.js
@@ -1,7 +1,4 @@
 import District from "./District.js";
 import Region from "./region/Region.js";
 
-export default class PD extends Region {
-  static SUB_REGION_OF = District;
-  static SUB_REGION_ID_KEY = "district_id";
-}
+export default class PD extends Region {}
