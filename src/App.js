8583273a238d40e244c64d5328cc59d6bec593d5
nuuuwan <nuuuwan@gmail.com>
2026-08-02 11:33:14 +0530

    fix: correct default visual query string format in App component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { AppTheme } from "./AppTheme";
 import AppFooter from "./view/atoms/AppFooter.js";
 const DEFAULT_VISUAL_QUERY_STR =
-  "/Person/Time+Province+Religion/Count/MarimekkoChart";
+  "/Person/Time=2024+Province+Religion/Count/MarimekkoChart";
 
 function App() {
   return (
