0abe5add4a9067494064347cbfe32b67d98c1e86
nuuuwan <nuuuwan@gmail.com>
2026-08-03 09:42:17 +0530

    fix: update default visual query string to use SquareMap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { AppTheme } from "./AppTheme";
 import AppFooter from "./view/atoms/AppFooter.js";
 import DataProvider from "./nonview/core/data_context/DataProvider.js";
 const DEFAULT_VISUAL_QUERY_STR =
-  "/Vote/ElectionType=presidential+Time=2024+PD<ED=colombo+Party/Count/HexMap";
+  "/Vote/ElectionType=presidential+Time=2024+PD<ED=colombo+Party/Count/SquareMap";
 
 function App() {
   return (
