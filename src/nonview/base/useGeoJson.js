f36507c86f52e09b93e5879f0655aa79208de167
nuuuwan <nuuuwan@gmail.com>
2026-08-04 15:27:17 +0530

    Refactor useGeoJson to improve caching and loading logic for GeoJSON data

--- src/nonview/base/useGeoJson.js
+++ src/nonview/base/useGeoJson.js
@@ -3,22 +3,53 @@ import { feature } from "topojson-client";
 
 import WWW from "./WWW.js";
 
+const geoJsonByURL = new Map();
+const geoJsonRequestByURL = new Map();
+
+async function loadGeoJson(geoURL) {
+  const cachedGeoJson = geoJsonByURL.get(geoURL);
+  if (cachedGeoJson) {
+    return cachedGeoJson;
+  }
+
+  if (!geoJsonRequestByURL.has(geoURL)) {
+    const request = WWW.json(geoURL)
+      .then((topoJson) => feature(topoJson, topoJson.objects.data))
+      .then((geoJson) => {
+        geoJsonByURL.set(geoURL, geoJson);
+        return geoJson;
+      })
+      .finally(() => geoJsonRequestByURL.delete(geoURL));
+    geoJsonRequestByURL.set(geoURL, request);
+  }
+
+  return geoJsonRequestByURL.get(geoURL);
+}
+
 export default function useGeoJson(regionClass) {
-  const [geoJson, setGeoJson] = useState(null);
+  const geoURL = regionClass.getGeoURL();
+  const [geoJson, setGeoJson] = useState(() => geoJsonByURL.get(geoURL));
 
   useEffect(() => {
     let cancelled = false;
+    const cachedGeoJson = geoJsonByURL.get(geoURL);
+    if (cachedGeoJson) {
+      setGeoJson(cachedGeoJson);
+      return undefined;
+    }
+
+    setGeoJson(null);
     async function load() {
-      const topoJson = await WWW.json(regionClass.getGeoURL());
+      const nextGeoJson = await loadGeoJson(geoURL);
       if (!cancelled) {
-        setGeoJson(feature(topoJson, topoJson.objects.data));
+        setGeoJson(nextGeoJson);
       }
     }
     load();
     return () => {
       cancelled = true;
     };
-  }, [regionClass]);
+  }, [geoURL]);
 
   return geoJson;
 }
